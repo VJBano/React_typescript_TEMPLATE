1e22e9312eb5fa7acfd799418c4a8794830c2345
vjvanz <vincentjaybano@gmail.com>
2022-10-29 08:27:33 +0800

    react-typescript template with tailwind css

--- README.md
+++ README.md
@@ -44,3 +44,8 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+## If problem occurs in your end
+
+Try to change "start": "react-scripts --openssl-legacy-provider start" inside package.json
+into "start": "react-scripts start"
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+                <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
@@ -16,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1 className='text-3xl font-bold underline'>Learn React</h1> 
         </a>
       </header>
     </div>
